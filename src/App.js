74308d53f318149342dc2c540bf600681fc2a86e
Nicolas DUBIEN <github@dubien.org>
2018-10-24 20:19:51 +0200

    Extract tokens into a separate file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /*global swal*/
+// @ts-check
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import loading from './loading.svg';
-import './App.css';
-import Sound from 'react-sound';
-import Button from './Button';
-
-const apiToken = '<<Copiez le token de Spotify ici>>';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import loading from "./loading.svg";
+import "./App.css";
+import Sound from "react-sound";
+import Button from "./Button";
+import { apiToken } from "./tokens";
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -29,28 +29,24 @@ function getRandomNumber(x) {
 }
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = { text: "" };
   }
-
   componentDidMount() {
     this.setState({ text: "Bonjour" });
   }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{ this.state.text }</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">{this.state.text}</h1>
         </header>
         <div className="App-images">
           <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
         </div>
-        <div className="App-buttons">
-        </div>
+        <div className="App-buttons" />
       </div>
     );
   }
